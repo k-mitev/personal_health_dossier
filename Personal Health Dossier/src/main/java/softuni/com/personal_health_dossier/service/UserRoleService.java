e2b82a1549d03cb2e35a36ec2c69919c9ef5c712
k-mitev <kristiyank.mitev@gmail.com>
2021-11-04 16:48:18 +0200

    added UserRoleService

--- Personal Health Dossier/src/main/java/softuni/com/personal_health_dossier/service/UserRoleService.java
+++ Personal Health Dossier/src/main/java/softuni/com/personal_health_dossier/service/UserRoleService.java
@@ -5,5 +5,6 @@ import softuni.com.personal_health_dossier.model.entities.enums.UserRoleEnum;
 
 public interface UserRoleService {
     UserRoleEntity findByUserRole(UserRoleEnum userRoleEnum);
+
     void seedRoles();
 }
